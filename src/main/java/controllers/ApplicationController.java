a5cfe0358e6f201899a2d48b2a15b957f2ab77c0
wuerfel <patrick.thum@student.uni-halle.de>
2013-08-10 22:15:03 +0200

    forms

--- src/main/java/controllers/ApplicationController.java
+++ src/main/java/controllers/ApplicationController.java
@@ -18,17 +18,22 @@ package controllers;
 
 import utils.DAOController;
 import models.User;
+import models.forms.UserFormData;
+import ninja.Context;
 import ninja.Result;
 import ninja.Results;
 import ninja.Router;
 import ninja.i18n.Lang;
+import ninja.validation.JSR303Validation;
+import ninja.validation.Validation;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.google.inject.persist.Transactional;
 
 @Singleton
-public class ApplicationController {
+public class ApplicationController
+{
 
 	/**
 	 * This is the system wide logger. You can still use any config you like. Or
@@ -44,12 +49,13 @@ public class ApplicationController {
 
 	@Inject
 	Lang lang;
-	
+
 	@Inject
 	DAOController dao;
 
-	public Result getIndex() {
-		
+	public Result getIndex()
+	{
+
 		User u = new User();
 		u.setCity("");
 		u.setEmail("email");
@@ -62,7 +68,7 @@ public class ApplicationController {
 		u.setStreetNo("streetNo");
 		u.setSurName("surName");
 		u.setTelephoneNo("12");
-		
+
 		User u2 = new User();
 		u2.setCity("ww");
 		u2.setEmail("emaiwwl");
@@ -75,21 +81,76 @@ public class ApplicationController {
 		u2.setStreetNo("stwreetNo");
 		u2.setSurName("surNwame");
 		u2.setTelephoneNo("1w2");
-		dao.<User>persist(u);
-		dao.<User>persist(u2);
-		dao.<User>remove(u);
+		dao.<User> persist(u);
+		dao.<User> persist(u2);
+		dao.<User> remove(u);
 		User returned = dao.getSingleElement(User.class, u2.getId());
 		System.out.println(returned);
 		return Results.html();
 
 	}
-	
-	public Result userForm(){
+
+	/**
+	 * Shows the User-Form GET /user
+	 * 
+	 * @return the user-form
+	 */
+	public Result userForm()
+	{
+		return Results.html();
+	}
+
+	/**
+	 * Takes the post of the User-Form POST /user
+	 * 
+	 * @return to the user-form
+	 */
+	public Result userPostForm(Context context, @JSR303Validation UserFormData userData, Validation validation)
+	{
+		if (validation.hasViolations())
+		{
+			context.getFlashCookie().error("violations!");
+		} else
+		{
+			logger.info(userData.printOut());
+			dao.<User>persist(userData.getAsUser());
+		}
+		return Results.redirect(context.getContextPath() + "/user");
+	}
+
+	/**
+	 * Shows the Account-Form GET /account
+	 * 
+	 * @return the account-form
+	 */
+	public Result accountForm()
+	{
+		return Results.html();
+	}
+
+	/**
+	 * Shows the Application-Form GET /account
+	 * 
+	 * @return the Application-form
+	 */
+	public Result applicationForm()
+	{
+		return Results.html();
+	}
+
+	/**
+	 * Shows the Finance-Plan-Form GET /account
+	 * 
+	 * @return the Finance-Plan-form
+	 */
+	public Result financePlanForm()
+	{
 		return Results.html();
 	}
 
 	@Transactional
-	public Result postIndex() {
+	public Result postIndex()
+	{
 
 		logger.info("In postRoute");
 		return Results.redirect("/");
--- src/main/java/models/forms/UserFormData.java
+++ src/main/java/models/forms/UserFormData.java
@@ -24,32 +24,40 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.Pattern;
 
 import models.Account;
 import models.Application;
 import models.ParticipantListItem;
+import models.User;
+import ninja.validation.Length;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
 import org.mindrot.jbcrypt.BCrypt;
 
 @Entity
-public class UserFormData {
+public class UserFormData
+{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
-
 	/**
 	 * the user's first name
 	 */
+	@NotEmpty
 	private String firstName;
 	/**
 	 * the user's surname
 	 */
+	@NotEmpty
 	private String surName;
 	/**
 	 * the user's matricle number
 	 */
+	@NotEmpty
 	private String matricleNo;
 	/**
 	 * the user's role (applicant, stura-member, etc.)
@@ -58,163 +66,242 @@ public class UserFormData {
 	/**
 	 * the user's password
 	 */
+	@NotEmpty
 	private String password;
+
+	/**
+	 * the user's password repetition
+	 */
+	@NotEmpty
+	private String password2;
 	/**
 	 * the user's telephone number
 	 */
+	@NotEmpty
 	private String telephoneNo;
 	/**
 	 * the user's postal code
 	 */
+	@NotEmpty
+	@Pattern(regexp = "\\d+")
 	private String postal;
 	/**
 	 * the user's street name
 	 */
+	@NotEmpty
 	private String street;
 	/**
 	 * the user's city
 	 */
+	@NotEmpty
 	private String city;
 	/**
 	 * the user's street number
 	 */
+	@NotEmpty
 	private String streetNo;
 	/**
 	 * the user's email-address
 	 */
+	@NotEmpty
+	@Email
 	private String email;
-	
-	public Long getId() {
+
+	public Long getId()
+	{
 		return id;
 	}
 
-	public void setId(Long id) {
+	public void setId(Long id)
+	{
 		this.id = id;
 	}
 
-	public List<Application> getApplications() {
+	public List<Application> getApplications()
+	{
 		return applications;
 	}
 
-	public void setApplications(List<Application> applications) {
+	public void setApplications(List<Application> applications)
+	{
 		this.applications = applications;
 	}
 
-	public List<Account> getAccounts() {
+	public List<Account> getAccounts()
+	{
 		return accounts;
 	}
 
-	public void setAccounts(List<Account> accounts) {
+	public void setAccounts(List<Account> accounts)
+	{
 		this.accounts = accounts;
 	}
-	
-	@OneToMany(cascade=CascadeType.ALL, mappedBy="applicant")
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "applicant")
 	private List<Application> applications;
-	
-	@OneToMany(cascade=CascadeType.ALL, mappedBy="user")
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
 	private List<Account> accounts;
-	
-	@OneToMany(cascade=CascadeType.ALL, mappedBy="participant")
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "participant")
 	private List<ParticipantListItem> participationList;
-	
-	public String getFirstName() {
+
+	public String getFirstName()
+	{
 		return firstName;
 	}
 
-	public void setFirstName(String firstName) {
+	public void setFirstName(String firstName)
+	{
 		this.firstName = firstName;
 	}
 
-	public String getSurName() {
+	public String getSurName()
+	{
 		return surName;
 	}
 
-	public void setSurName(String surName) {
+	public void setSurName(String surName)
+	{
 		this.surName = surName;
 	}
 
-	public String getMatricleNo() {
+	public String getMatricleNo()
+	{
 		return matricleNo;
 	}
 
-	public void setMatricleNo(String matricleNo) {
+	public void setMatricleNo(String matricleNo)
+	{
 		this.matricleNo = matricleNo;
 	}
 
-	public Integer getRole() {
+	public Integer getRole()
+	{
 		return role;
 	}
 
-	public void setRole(Integer role) {
+	public void setRole(Integer role)
+	{
 		this.role = role;
 	}
 
-	public String getPassword() {
+	public String getPassword()
+	{
 		return password;
 	}
 
-	public void setPassword(String password) {
+	public void setPassword(String password)
+	{
 		this.password = password;
 	}
 
-	public String getTelephoneNo() {
+	public String getPassword2()
+	{
+		return password2;
+	}
+
+	public void setPassword2(String password2)
+	{
+		this.password2 = password2;
+	}
+
+	public String getTelephoneNo()
+	{
 		return telephoneNo;
 	}
 
-	public void setTelephoneNo(String telephoneNo) {
+	public void setTelephoneNo(String telephoneNo)
+	{
 		this.telephoneNo = telephoneNo;
 	}
 
-	public String getPostal() {
+	public String getPostal()
+	{
 		return postal;
 	}
 
-	public void setPostal(String postal) {
+	public void setPostal(String postal)
+	{
 		this.postal = postal;
 	}
 
-	public String getStreet() {
+	public String getStreet()
+	{
 		return street;
 	}
 
-	public void setStreet(String street) {
+	public void setStreet(String street)
+	{
 		this.street = street;
 	}
 
-	public String getCity() {
+	public String getCity()
+	{
 		return city;
 	}
 
-	public void setCity(String city) {
+	public void setCity(String city)
+	{
 		this.city = city;
 	}
 
-	public String getStreetNo() {
+	public String getStreetNo()
+	{
 		return streetNo;
 	}
 
-	public void setStreetNo(String streetNo) {
+	public void setStreetNo(String streetNo)
+	{
 		this.streetNo = streetNo;
 	}
 
-	public UserFormData() {
+	public UserFormData()
+	{
 	}
 
-	public String getEmail() {
+	public String getEmail()
+	{
 		return email;
 	}
 
-	public void setEmail(String email) {
+	public void setEmail(String email)
+	{
 		this.email = email;
 	}
 
-	public void hashPassword(String plainPass) {
+	public void hashPassword(String plainPass)
+	{
 		String crypt = BCrypt.hashpw(plainPass, BCrypt.gensalt());
 		this.setPassword(crypt);
 	}
 
-	public boolean checkPw(String passWord) {
+	public boolean checkPw(String passWord)
+	{
 		return BCrypt.checkpw(passWord, this.getPassword());
+	}
+
+	public String printOut()
+	{
+		return firstName + ", " + surName + ", " + matricleNo + ", " + role + ", " + password + ", ";
+	}
+	
+	public User getAsUser(){
+		User user = new User();
+		
+		user.setFirstName(firstName);
+		user.setCity(city);
+		user.setEmail(email);
+		user.setMatricleNo(matricleNo);
+		user.setPassword(password);
+		user.setPostal(postal);
+		user.setStreet(street);
+		user.setStreetNo(streetNo);
+		user.setSurName(surName);
+		user.setTelephoneNo(telephoneNo);
+		
+		return user;
+		
 	}
 
 }
